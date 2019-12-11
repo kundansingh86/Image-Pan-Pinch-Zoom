109731627f799f7fe20fa11d7e74a4035ea5ebac
Kundan Singh Chouhan <kundansingh143@gmail.com>
2019-12-12 00:18:06 +0530

    added controller js and set constraint for images

--- touchScript.js
+++ touchScript.js
@@ -1,5 +1,6 @@
 var element = document.getElementById('imageZoom');
-var parent = document.getElementById('container');
+var parentElement = document.getElementById('container');
+var label = document.getElementById('lblConsole');
 
 var mouseDown = false, scaling = false;
 var startX = 0, startY = 0;
@@ -8,11 +9,33 @@ var adjustScale = 1, adjustDeltaX = 0, adjustDeltaY = 0;
 var currentScale = 1, currentDeltaX = 0, currentDeltaY = 0;
 var tapTimestamp = 0;
 
+var hypo = undefined;
+
 
 function addListenerMulti(elem, eventNames, listener) {
     var events = eventNames.split(' ');
     for (var i=0, iLen=events.length; i<iLen; i++) {
         elem.addEventListener(events[i], listener, false);
+    }
+}
+
+function setConstraints() {
+    
+    let minDeltaX = ((element.width * currentScale - parentElement.clientWidth) / (currentScale * 2));
+    let minDeltaY = ((element.height * currentScale - parentElement.clientHeight) / (currentScale * 2));
+    
+    if (currentScale < 1) {
+        currentScale = 1;
+        currentDeltaX = 0;
+        currentDeltaY = 0;
+    } 
+    
+    if(Math.abs(currentDeltaX) > minDeltaX) {
+        currentDeltaX = minDeltaX * Math.sign(currentDeltaX);
+    }
+
+    if(Math.abs(currentDeltaY) > minDeltaY) {
+        currentDeltaY = minDeltaY * Math.sign(currentDeltaY);
     }
 }
 
@@ -29,15 +52,16 @@ function setTransform() {
   
 addListenerMulti(element, 'touchstart mousedown', function(e) {
     e.preventDefault();
-    console.log(e.type, e.touches.length);
+    console.log(e.type, e);
+
+    label.innerText += `${e.type} \n`;
+
     mouseDown = (e.type === 'mousedown');
 
     startX = e.clientX || e.touches[0].clientX;
-    startY = e.clientY || e.touches[0].clientY;
-
-    if (e.touches && e.touches.length === 2) {
-        scaling = true;
-    }
+    startY = e.clientY || e.touches[0].clientY;   
+    
+    scaling = (e.touches && e.touches.length === 2);
 });
 
 addListenerMulti(element, 'touchmove mousemove', function(e) {
@@ -51,55 +75,60 @@ addListenerMulti(element, 'touchmove mousemove', function(e) {
 
 
     if(scaling) {
-        var scale = Math.hypot(
-            e.touches[0].pageX - e.touches[1].pageX,
-            e.touches[0].pageY - e.touches[1].pageY);
+        scale = currentScale;
+        deltaX = 0, deltaY = 0, lastDeltaX = 0;
+        let hypoTouchMove = Math.hypot(
+            e.touches[0].clientX - e.touches[1].clientX,
+            e.touches[0].clientY - e.touches[1].clientY);
+
+        if(hypo === undefined) {
+            hypo = hypoTouchMove;
+        }
+
+        scale = hypoTouchMove / hypo;
     }
 
     currentScale = adjustScale * scale;
     currentDeltaX = adjustDeltaX + (deltaX / currentScale);
     currentDeltaY = adjustDeltaY + (deltaY / currentScale);
-    if (currentScale < 1) {
-        currentScale = 1;
-        currentDeltaX = 0;
-        currentDeltaY = 0;
-    }
 
+    label.innerText += `${currentScale}\n`;
+
+    setConstraints();
     setTransform();
 });
 
-addListenerMulti(element, 'touchend mouseup mouseout', function(e) {
+addListenerMulti(element, 'touchend mouseup', function(e) {
     e.preventDefault();
-    mouseDown = !(e.type === 'mouseup' || e.type === 'mouseout');
+    mouseDown = !(e.type === 'mouseup');
     console.log(e.type, e);
-
-    let diff = (e.timeStamp - tapTimestamp);
     
-    if(diff < 500) {
-        currentScale += 0.5;
-        if(currentScale > 3) {
-            currentScale = 1;
-        }
-
-        setTransform();
-    }
-
-    if (scaling) {
-        scaling = false;
-    }
-
     adjustScale = currentScale;
     adjustDeltaX = currentDeltaX;
     adjustDeltaY = currentDeltaY;
     tapTimestamp = e.timeStamp;
+    hypo = undefined;
+    scaling = false;
 });
 
-addListenerMulti(element, 'touchcancel', function(e) {
+addListenerMulti(element, 'wheel', function(e) {
+    e.preventDefault();
+    currentScale += event.deltaY * -0.01;
+    adjustScale = currentScale;
+    setConstraints();
+    setTransform();
+});
+
+addListenerMulti(element, 'touchcancel mouseout', function(e) {
     e.preventDefault();
     console.log(e.type, e);
+    label.innerText += `${e.type} \n`;
 
     adjustScale = currentScale;
     adjustDeltaX = currentDeltaX;
     adjustDeltaY = currentDeltaY;
+    hypo = undefined;
+    scaling = false;
+    mouseDown = false;
 });
 
---
+++ touchScriptController.js
@@ -0,0 +1,153 @@
+function touchScriptController(element, parentElement) {
+    var mouseDown = false, scaling = false;
+    var startX = 0, startY = 0;
+    
+    var adjustScale = 1, adjustDeltaX = 0, adjustDeltaY = 0;
+    var currentScale = 1, currentDeltaX = 0, currentDeltaY = 0;
+    var tapTimestamp = 0;
+
+    var hypo = undefined;
+
+    function addListenerMulti(elem, eventNames, listener) {
+        var events = eventNames.split(' ');
+        for (var i=0, iLen=events.length; i<iLen; i++) {
+            elem.addEventListener(events[i], listener, false);
+        }
+    }
+
+    function setTransform() {
+        let transforms = [];
+    
+        transforms.push(`scale(${currentScale})`);
+        transforms.push(`translate(${currentDeltaX}px, ${currentDeltaY}px)`);
+    
+        element.style.transform = transforms.join(' ');
+    }
+
+    function setConstraints() {
+    
+        let minDeltaX = ((element.width * currentScale - parentElement.clientWidth) / (currentScale * 2));
+        let minDeltaY = ((element.height * currentScale - parentElement.clientHeight) / (currentScale * 2));
+        
+        if (currentScale < 1) {
+            currentScale = 1;
+            currentDeltaX = 0;
+            currentDeltaY = 0;
+        } 
+        
+        if(Math.abs(currentDeltaX) > minDeltaX) {
+            currentDeltaX = minDeltaX * Math.sign(currentDeltaX);
+        }
+    
+        if(Math.abs(currentDeltaY) > minDeltaY) {
+            currentDeltaY = minDeltaY * Math.sign(currentDeltaY);
+        }
+    }
+
+    function bindEvents() {
+        addListenerMulti(element, 'touchstart mousedown', handleTouchStart);
+        
+        addListenerMulti(element, 'touchmove mousemove', handleTouchMove);
+        
+        addListenerMulti(element, 'touchend mouseup', handleTouchEnd);
+        
+        addListenerMulti(element, 'touchcancel mouseout', handleTouchCancel);
+        
+        addListenerMulti(element, 'wheel', handleWheel);
+    }
+    
+    function handleTouchStart(e) {
+        e.preventDefault();
+        
+        mouseDown = (e.type === 'mousedown');
+    
+        startX = e.clientX || e.touches[0].clientX;
+        startY = e.clientY || e.touches[0].clientY;   
+        
+        scaling = (e.touches && e.touches.length === 2);
+    }
+
+    function handleTouchMove(e) {
+        e.preventDefault();
+        if(e.type === 'mousemove' && !mouseDown) { return false; }
+    
+        var deltaX, deltaY, scale = 1;
+    
+        deltaX = (e.clientX || e.changedTouches[0].clientX) - startX;
+        deltaY = (e.clientY || e.changedTouches[0].clientY) - startY;
+    
+    
+        if(scaling) {
+            scale = currentScale;
+            deltaX = 0, deltaY = 0, wasScaling = true;
+            let hypoTouchMove = Math.hypot(
+                e.touches[0].clientX - e.touches[1].clientX,
+                e.touches[0].clientY - e.touches[1].clientY);
+    
+            if(hypo === undefined) {
+                hypo = hypoTouchMove;
+            }
+    
+            scale = hypoTouchMove / hypo;
+        }         
+        
+        currentScale = adjustScale * scale;
+        currentDeltaX = adjustDeltaX + (deltaX / currentScale);
+        currentDeltaY = adjustDeltaY + (deltaY / currentScale);
+        if (currentScale < 1) {
+            currentScale = 1;
+            currentDeltaX = 0;
+            currentDeltaY = 0;
+        }
+    
+        setConstraints();
+        setTransform();
+    }
+
+    function handleTouchEnd(e) {
+        e.preventDefault();
+        mouseDown = !(e.type === 'mouseup');
+        
+        let diff = (e.timeStamp - tapTimestamp);
+        
+        if(diff > 100 && diff < 500) {
+            currentScale += 0.5;
+            if(currentScale > 3) {
+                currentScale = 1;
+            }
+    
+            setTransform();
+        }
+    
+        adjustScale = currentScale;
+        adjustDeltaX = currentDeltaX;
+        adjustDeltaY = currentDeltaY;
+        tapTimestamp = e.timeStamp;
+        hypo = undefined;
+        scaling = false;
+    }
+
+    function handleTouchCancel(e) {
+        e.preventDefault();
+        
+        adjustScale = currentScale;
+        adjustDeltaX = currentDeltaX;
+        adjustDeltaY = currentDeltaY;
+        hypo = undefined;
+        scaling = false;
+        mouseDown = false;
+    }
+
+    function handleWheel(e) {
+        currentScale += event.deltaY * -0.01;
+        adjustScale = currentScale;
+        setConstraints();
+        setTransform();
+    }
+    
+    function init() {
+        bindEvents();
+    }
+
+    init();
+}
